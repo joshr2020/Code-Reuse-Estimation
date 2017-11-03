50ca2b0b097ebb9e4b52651d48a9a5edc5bd9d95
Joshua Russo <jrusso@csail.mit.edu>
2017-11-03 10:50:24 -0400

    putting it all together

--- Demo_Materials/integratedProgram.py
+++ Demo_Materials/integratedProgram.py
@@ -1,8 +1,3 @@
-
-# coding: utf-8
-
-# In[5]:
-
 import json
 import io
 import sys
@@ -10,11 +5,54 @@ import csv
 import pandas as pd
 import subprocess
 from itertools import islice
+from pprint import pprint
+
+#x is a json filename
+# def compileDict(x):
+#     with io.open(x, 'r', encoding='utf-8-sig') as filex:
+#         datax = json.load(filex)
+#     dictx = {}
+#     for obj in datax:
+#         dictx[obj['Symbol_Name']] = obj['Size']
+#     return dictx
+
+def dfToDict(x):
+    dictx = {}
+    for i in x:
+        dictx[x[i]['Symbol_Name']] = x[i]['Size']
+    pprint(dictx)
+
+#x is a dictionary
+def getTotalSize(x):
+    x_total = 0
+    for size in x:
+        x_total += int(x.get(size), 16)
+    return x_total
+
+#x and y are both dictionaries holding data of executables to compare
+def compare(x, y):
+    keysx = set(x.keys())
+    keysy = set(y.keys())
+    intersection = keysx.intersection(keysy)
+    assert intersection == keysy.intersection(keysx)
+    x_total = getTotalSize(x)
+    y_total = getTotalSize(y)
+
+    overlap_size = 0
+    for key in intersection:
+        overlap_size += int(x.get(key), 16)
+
+    result = float(overlap_size) / float(x_total + y_total - overlap_size)
+    return result
+
+#x and y are both json filenames
+def calculateOverlap(x, y):
+    print("Overlap: ", compare(x, y))
 
 def gatherNMDump():
     exe = input('Enter the name of the executable: ')
-    filepath = "../testecutables/" + exe  
-    write_out = open(exe + "_dump.txt", "w") 
+    filepath = "../testecutables/" + exe
+    write_out = open(exe + "_dump.txt", "w")
     subprocess.run(['nm', '-S', filepath], stdout=write_out)
     fileName = write_out.name
     write_out.close()
@@ -33,12 +71,11 @@ def textToDataFrame(fileName):
 
     outfilename.close()
 
-   
+
     df = pd.read_table(outfilename.name, header = None, delim_whitespace=True)
     df.columns = ['Address', 'Size', 'Type', 'Symbol_Name']
     df.reset_index().to_json(orient='records')
-
-    print(df.ix[0])
+    return df
 
 def main():
     firstFH = gatherNMDump()
@@ -46,16 +83,6 @@ def main():
 
     df = textToDataFrame(firstFH)
     df2 = textToDataFrame(secondFH)
+    dfToDict(df)
 
 if __name__ == "__main__": main()
-
-
-# In[ ]:
-
-
-
-
-# In[ ]:
-
-
-
